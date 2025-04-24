406d4530d65354f6b6e5e91b98938779e583c3b1
sueun <absorbme@naver.com>
2025-04-24 09:46:35 +0900

    증가감소연산자예제

--- example/example.c
+++ example/example.c
@@ -3,8 +3,17 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    int num1 = 12;
+    int num2 = 12;
+    
+    printf("num1: %d\n", num1);
+    printf("num1++: %d\n", num1++); //후위 증가
+    printf("num1: %d\n", num1); //증가 된 값 
 
+    printf("num2: %d\n", num2);
+    printf("++num2: %d\n", ++num2);
+    printf("num2: %d", num2);
+   
     return 0;
 }
 
